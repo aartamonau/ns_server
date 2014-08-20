6c6197dcd1ea5752e6b8135d6bc05a9c4effe6df
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-08-20 15:07:01 -0700

    watches
    Change-Id: Iec183c0c5fe649a7444099dd59f9979861d3312e

--- src/config.erl
+++ src/config.erl
@@ -4,6 +4,7 @@
 
 -export([start_link/2]).
 -export([get/1, create/2, set/2, set/3, delete/1, delete/2]).
+-export([watch/1, unwatch/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          code_change/3, terminate/2]).
 
@@ -12,11 +13,12 @@
 -type cfg_version() :: any().
 -type cfg_state() :: any().
 -type cfg_error() :: any().
+-type cfg_reason() :: any().
 
 -callback init(Args :: any()) ->
     {ok, cfg_state()} | {error, cfg_error()}.
 
--callback terminate(cfg_state()) ->
+-callback terminate(cfg_reason(), cfg_state()) ->
     ok.
 
 -callback handle_get(cfg_path(), reference(), cfg_state()) ->
@@ -48,7 +50,8 @@
 
 -record(state, { backend :: module(),
                  backend_state :: any(),
-                 requests :: ets:tid() }).
+                 requests :: ets:tid(),
+                 watches :: ets:tid() }).
 
 start_link(Backend, Args) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, {Backend, Args}, []).
@@ -81,12 +84,19 @@ delete(Path) ->
 delete(Path, Version) ->
     gen_server:call(?MODULE, {delete, Path, Version}, infinity).
 
+watch(Paths) ->
+    gen_server:call(?MODULE, {watch, Paths}, infinity).
+
+unwatch(WatchRef) ->
+    gen_server:call(?MODULE, {unwatch, WatchRef}, infinity).
+
 init({Backend, Args}) ->
     case Backend:init(Args) of
         {ok, BackendState} ->
             {ok, #state{backend = Backend,
                         backend_state = BackendState,
-                        requests = ets:new(ok, [set, protected])}};
+                        requests = ets:new(ok, [set, protected]),
+                        watches = ets:new(ok, [set, protected])}};
         {error, _} = Error ->
             Error
     end.
@@ -102,32 +112,48 @@ handle_call({set, Path, Value, Version}, From, State) ->
 handle_call({delete, Path}, From, State) ->
     delegate_call(handle_delete, [Path], From, State);
 handle_call({delete, Path, Version}, From, State) ->
-    delegate_call(handle_delete, [Path, Version], From, State).
+    delegate_call(handle_delete, [Path, Version], From, State);
+handle_call({watch, Paths}, {FromPid, _} = From,
+            #state{watches = Watches} = State) ->
+    WatchRef = erlang:monitor(process, FromPid),
+    true = ets:insert_new(Watches, {WatchRef, FromPid}),
+    delegate_call(handle_watch, [Paths, WatchRef], From, State);
+handle_call({unwatch, WatchRef}, From, #state{watches = Watches} = State) ->
+    ets:delete(Watches, WatchRef),
+    delegate_call(handle_unwatch, [WatchRef], From, State);
+handle_call(Request, From, State) ->
+    ?log_warning("Got unknown call ~p from ~p", [Request, From]),
+    {reply, unknown_call, State}.
 
 handle_cast(_, State) ->
     {noreply, State}.
 
-handle_info(Msg, #state{backend = Backend,
-                        backend_state = BackendState} = State) ->
-    case Backend:handle_msg(Msg, BackendState) of
-        {noreply, NewBackendState} ->
-            {noreply, State#state{backend_state = NewBackendState}};
-        {reply, Tag, RV, NewBackendState} ->
-            reply(Tag, RV, State),
-            {noreply, State#state{backend_state = NewBackendState}};
-        ignore ->
-            ?log_warning("got unexpected message ~p", [Msg]),
-            {noreply, State};
-        {stop, Reason, State} ->
-            {stop, Reason, State}
-    end.
+handle_info({'DOWN', Ref, process, _, Reason} = Msg,
+            #state{watches = Watches} = State) ->
+    case ets:lookup(Watches, Ref) of
+        [] ->
+            handle_other_msg(Msg, State);
+        [{Ref, Pid}] ->
+            ?log_debug("Removing watch ~p because ~p died with reason ~p",
+                       [Ref, Pid, Reason]),
+            %% TODO: don't expect this to reply immediately
+            {reply, _, _} = handle_call({unwatch, Ref}, unused, State)
+    end;
+handle_info(Msg, State) ->
+    handle_other_msg(Msg, State).
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-terminate(_Reason, #state{backend = Backend,
-                          backend_state = BackendState}) ->
-    ok = Backend:terminate(BackendState).
+terminate(Reason, #state{backend = Backend,
+                         backend_state = BackendState,
+                         watches = Watches}) ->
+    ok = Backend:terminate(Reason, BackendState),
+
+    lists:foreach(
+      fun ({WatchRef, Pid}) ->
+              Pid ! {watch_lost, WatchRef}
+      end, ets:tab2list(Watches)).
 
 reply(Tag, RV, #state{requests = Requests}) ->
     [{Tag, From}] = ets:lookup(Requests, Tag),
@@ -137,7 +163,6 @@ reply(Tag, RV, #state{requests = Requests}) ->
 delegate_call(Call, Args, From, #state{backend = Backend,
                                        backend_state = BackendState,
                                        requests = Requests} = State) ->
-
     Tag = make_ref(),
     case erlang:apply(Backend, Call, Args ++ [Tag, BackendState]) of
         {noreply, NewBackendState} ->
@@ -145,4 +170,27 @@ delegate_call(Call, Args, From, #state{backend = Backend,
             {noreply, State#state{backend_state = NewBackendState}};
         {reply, Reply, NewBackendState} ->
             {reply, Reply, State#state{backend_state = NewBackendState}}
+    end.
+
+notify_watch(WatchRef, Path, #state{watches = Watches}) ->
+    [{WatchRef, Pid}] = ets:lookup(Watches, WatchRef),
+    Pid ! {watch, WatchRef, Path},
+    ok.
+
+handle_other_msg(Msg, #state{backend = Backend,
+                             backend_state = BackendState} = State) ->
+    case Backend:handle_msg(Msg, BackendState) of
+        {noreply, NewBackendState} ->
+            {noreply, State#state{backend_state = NewBackendState}};
+        {reply, Tag, RV, NewBackendState} ->
+            reply(Tag, RV, State),
+            {noreply, State#state{backend_state = NewBackendState}};
+        {notify_watch, WatchRef, Path, NewBackendState} ->
+            notify_watch(WatchRef, Path, State),
+            {noreply, State#state{backend_state = NewBackendState}};
+        ignore ->
+            ?log_warning("Got unexpected message ~p", [Msg]),
+            {noreply, State};
+        {stop, Reason, State} ->
+            {stop, Reason, State}
     end.
--- src/config_ezk.erl
+++ src/config_ezk.erl
@@ -2,27 +2,39 @@
 
 -behaviour(config).
 
--export([init/1, terminate/1]).
+-export([init/1, terminate/2]).
 -export([handle_get/3, handle_create/4, handle_set/4, handle_set/5,
-         handle_delete/3, handle_delete/4]).
+         handle_delete/3, handle_delete/4, handle_watch/4, handle_unwatch/3]).
 -export([handle_msg/2]).
 
 -include_lib("ezk/include/ezk.hrl").
+-include("ns_common.hrl").
 
--record(state, { connection :: ezk_conpid() }).
+-record(state, { connection :: ezk_conpid(),
+                 watches :: ets:tid() }).
+
+-record(pending_watch, { paths_left :: [config:cfg_path()],
+                         paths_triggered :: [config:cfg_path()] }).
 
 init(_Args) ->
     application:start(ezk),
     %% TODO: get configuration from Args
     case ezk:start_connection([], [self()]) of
         {ok, Conn} ->
-            {ok, #state{connection = Conn}};
+            erlang:monitor(process, Conn),
+            {ok, #state{connection = Conn,
+                        watches = ets:new(ok, [set, protected])}};
         {error, Error} ->
             {error, {cant_create_ezk_connection, Error}}
     end.
 
-terminate(#state{connection = Conn}) ->
-    ok = ezk:end_connection(Conn, "terminating").
+terminate(_Reason, #state{connection = Conn}) ->
+    case Conn =/= undefined of
+        true ->
+            ezk:end_connection(Conn, "terminating");
+        false ->
+            ok
+    end.
 
 handle_get(Path, Tag, #state{connection = Conn} = State) ->
     ok = ezk:n_sync(Conn, Path, self(), {synced, Path, Tag}),
@@ -51,6 +63,23 @@ handle_delete(Path, Version, Tag, #state{connection = Conn} = State) ->
     ok = ezk:n_delete(Conn, Path, Version, self(), {reply, delete, Tag}),
     {noreply, State}.
 
+handle_watch(Paths, WatchRef, Tag, #state{connection = Conn,
+                                          watches = Watches} = State) ->
+    lists:foreach(
+      fun (Path) ->
+              ok = ezk:n_exists(Conn, Path, self(),
+                                {watch, Path, WatchRef},
+                                {watch_reply, Path, Tag, WatchRef})
+      end, Paths),
+    true = ets:insert_new(Watches,
+                          {WatchRef, #pending_watch{paths_left = Paths,
+                                                    paths_triggered = []}}),
+    {noreply, State}.
+
+handle_unwatch(WatchRef, Tag, #state{watches = Watches} = State) ->
+    ets:delete(WatchRef, Watches),
+    {reply, Tag, ok, State}.
+
 handle_msg({{synced, Path, Tag}, RV}, #state{connection = Conn} = State) ->
     case RV of
         {ok, _Path} ->
@@ -61,6 +90,62 @@ handle_msg({{synced, Path, Tag}, RV}, #state{connection = Conn} = State) ->
     end;
 handle_msg({{reply, ReplyType, Tag}, RV}, State) ->
     {reply, Tag, translate_reply(ReplyType, RV), State};
+handle_msg({{watch_reply, Path, Tag, WatchRef}, RV} = Msg,
+           #state{watches = Watches} = State) ->
+    case ets:lookup(Watches, WatchRef) of
+        [{WatchRef, #pending_watch{paths_left = Paths,
+                                   paths_triggered = Triggered}}] ->
+            case RV of
+                {error, Error} when Error =/= no_node ->
+                    ets:delete(Watches, WatchRef),
+                    {reply, Tag, {error, translate_error(Error)}, State};
+                _ ->
+                    case lists:delete(Path, Paths) of
+                        [] ->
+                            ets:insert(Watches, {WatchRef, initialized}),
+                            [self() ! {{watch, P, WatchRef}, unused}
+                             || P <- Triggered],
+                            {reply, Tag, ok, State};
+                        NewPaths ->
+                            ets:insert(Watches,
+                                       {WatchRef,
+                                        #pending_watch{paths_left = NewPaths}}),
+                            {noreply, State}
+                    end
+            end;
+        [] ->
+            ?log_debug("Ignoring watch_reply with unknown tag: ~p", [Msg]),
+            {noreply, State}
+    end;
+handle_msg({{watch_rearm_reply, Path}, RV}, State) ->
+    case RV of
+        {error, Error} when Error =/= no_node ->
+            {stop, {couldnt_rearm_watch, Path, translate_error(Error)}, State};
+        _ ->
+            {noreply, State}
+    end;
+handle_msg({'DOWN', _, process, Conn, Reason},
+           #state{connection = Conn} = State) ->
+    ?log_error("Lost connection to zookeeper: ~p. Terminating", [Reason]),
+    {stop, {ezk_connection_lost, Reason}, State#state{connection = undefined}};
+handle_msg({{watch, Path, WatchRef}, _} = Msg,
+           #state{connection = Conn,
+                  watches = Watches} = State) ->
+    case ets:lookup(Watches, WatchRef) of
+        [] ->
+            ?log_debug("Ignoring notification for non-existent watch ~p", [Msg]),
+            {noreply, State};
+        [{WatchRef, initialized}] ->
+            ok = ezk:n_exists(Conn, Path, self(),
+                              {watch, Path, WatchRef},
+                              {watch_rearm_reply, Path}),
+            {notify_watch, WatchRef, Path, State};
+        [{WatchRef, #pending_watch{paths_triggered = Triggered}}] ->
+            ets:insert(Watches,
+                       {WatchRef,
+                        #pending_watch{paths_triggered = [Path | Triggered]}}),
+            {noreply, State}
+    end;
 handle_msg(_, _State) ->
     ignore.
 
